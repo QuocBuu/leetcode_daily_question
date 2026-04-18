71c92a3677fbc0ac4ec648a0ece160610906226e
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-04-18 18:32:07 +0700

    Time: 0 ms (100.00%) | Memory: 9.2 MB (22.70%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.c
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.c
@@ -2,10 +2,9 @@ int mirrorDistance(int n) {
     int m = 0;
     int k = n;
     while (k > 0) {
-        int q = k % 10;
-        m = m*10 + q;
+        m = m*10 + k % 10;
         k /= 10;
     }
 
-    return abs(m - n);
+    return m > n ? (m - n) : (n - m);
 }
