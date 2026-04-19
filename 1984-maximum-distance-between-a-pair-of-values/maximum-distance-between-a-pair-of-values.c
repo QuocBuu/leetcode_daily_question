beaace120abe2cc94a9fcd79620a5a35f963a45c
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-04-19 21:39:10 +0700

    Time: 0 ms (100.00%) | Memory: 16.9 MB (66.67%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.c
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.c
@@ -3,18 +3,21 @@ int maxDistance(int* nums1, int nums1Size, int* nums2, int nums2Size) {
     int ret = 0;
 
     while (i < nums1Size && j < nums2Size) {
-        if (i > j) {
-            j = i;
-        }
+        // if (i > j) {
+        //     j = i;
+        // }
 
-        if (j >= nums2Size) {
-            break;
-        }
+        // if (j >= nums2Size) {
+        //     break;
+        // }
 
         if (nums1[i] <= nums2[j]) {
-            if ((j - i) > ret) {
-                ret = j - i;
-            } 
+            if (i < j) {
+                int diff = j - i;
+                if (diff > ret) {
+                    ret = diff;
+                } 
+            }
             j++;
         }
         else {
