e997b28a41a1fbfbcdbfa439c35202d799dbcd7d
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-05-04 18:50:44 +0700

    Time: 0 ms (100.00%) | Memory: 11.1 MB (5.93%) - LeetSync

--- 48-rotate-image/rotate-image.c
+++ 48-rotate-image/rotate-image.c
@@ -4,12 +4,19 @@ void rotate(int** matrix, int matrixSize, int* matrixColSize) {
         int last = matrixSize - 1 - k;
         for (int j = k; j < last; j++) {
             int offset = j - k;
-            int temp = matrix[k][j];
-
-            matrix[k][j] = matrix[last - offset][k];
-            matrix[last - offset][k] = matrix[last][last - offset];
-            matrix[last][last - offset] = matrix[j][last];
-            matrix[j][last] = temp;
+            int x1 = k;
+            int y1 = j;
+            int x2 = j;
+            int y2 = last;
+            int x3 = last;
+            int y3 = last - offset;
+            int x4 = last - offset;
+            int y4 = k;
+            int temp = matrix[x1][y1];
+            matrix[x1][y1] = matrix[x4][y4];
+            matrix[x4][y4] = matrix[x3][y3];
+            matrix[x3][y3] = matrix[x2][y2];
+            matrix[x2][y2] = temp;
         }
     }
 }
