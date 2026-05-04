b4c1b61cbeb969a29e600c01995bc9d1d06b0972
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-05-04 18:49:48 +0700

    Time: 0 ms (100.00%) | Memory: 11.2 MB (5.93%) - LeetSync

--- 48-rotate-image/rotate-image.c
+++ 48-rotate-image/rotate-image.c
@@ -5,24 +5,31 @@ void rotate(int** matrix, int matrixSize, int* matrixColSize) {
         for (int j = k; j < last; j++) {
             int offset = j - k;
 
-            int x1 = k;
-            int y1 = j;
+            // int x1 = k;
+            // int y1 = j;
 
-            int x2 = j;
-            int y2 = last;
+            // int x2 = j;
+            // int y2 = last;
 
-            int x3 = last;
-            int y3 = last - offset;
+            // int x3 = last;
+            // int y3 = last - offset;
 
-            int x4 = last - offset;
-            int y4 = k;
+            // int x4 = last - offset;
+            // int y4 = k;
 
-            int temp = matrix[x1][y1];
+            // int temp = matrix[x1][y1];
 
-            matrix[x1][y1] = matrix[x4][y4];
-            matrix[x4][y4] = matrix[x3][y3];
-            matrix[x3][y3] = matrix[x2][y2];
-            matrix[x2][y2] = temp;
+            // matrix[x1][y1] = matrix[x4][y4];
+            // matrix[x4][y4] = matrix[x3][y3];
+            // matrix[x3][y3] = matrix[x2][y2];
+            // matrix[x2][y2] = temp;
+
+            int temp = matrix[k][j];
+
+            matrix[k][j] = matrix[last - offset][k];
+            matrix[last - offset][k] = matrix[last][last - offset];
+            matrix[last][last - offset] = matrix[j][last];
+            matrix[j][last] = temp;
         }
     }
 }
