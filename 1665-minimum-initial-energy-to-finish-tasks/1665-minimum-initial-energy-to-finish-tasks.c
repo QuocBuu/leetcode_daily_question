72a3bb62428d964bf7c3c6415201f34f5c925c5e
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-05-12 20:36:27 +0700

    Time: 31 ms (33.33%), Space: 36.6 MB (0%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.c
@@ -0,0 +1,24 @@
+int cmp (const void* a, const void* b) {
+    int* arr1 = *(int**)a;
+    int* arr2 = *(int**)b;
+    return (arr1[1] - arr1[0]) - (arr2[1] - arr2[0]);
+}
+int minimumEffort(int** tasks, int tasksSize, int* tasksColSize) {
+    int m = tasksSize;
+    int n = tasksColSize[0];
+
+    qsort(tasks, tasksSize, sizeof(int*), cmp);
+
+    int ret = 0;
+    for (int i = 0; i < tasksSize; i++) {
+        // printf("%d\n", tasks[i][1]);
+        if ((ret + tasks[i][0]) < tasks[i][1]) {
+            ret = tasks[i][1];
+        }
+        else {
+            ret += tasks[i][0];
+        }
+    }
+
+    return ret;
+}
