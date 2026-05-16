d54d4bc8c694d21835bd9b2b785b6ef58cb94b05
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-05-16 23:10:45 +0700

    Time: 47 ms (7.48%), Space: 12.1 MB (18.87%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.c
@@ -0,0 +1,24 @@
+int cmp (const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
+}
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int size = nums1Size+nums2Size;
+    int array[size];
+
+    memcpy(array, nums1, nums1Size * sizeof(int));
+    memcpy(array+nums1Size, nums2, nums2Size * sizeof(int));
+
+    qsort(array, nums1Size + nums2Size, sizeof(int), cmp);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d - %d\n", array[i], size);
+    }
+
+    int halfSize = size/2; 
+    if (size % 2 == 0) {
+        return ((double)(array[halfSize] + array[halfSize-1])) / 2;
+    }
+
+    return (double)array[halfSize];
+}
