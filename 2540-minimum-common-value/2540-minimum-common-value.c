ed5330362049e99d7ed1a128125373f6387a88e2
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-05-19 09:00:24 +0700

    Time: 0 ms (100%), Space: 15 MB (92.74%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.c
+++ 2540-minimum-common-value/2540-minimum-common-value.c
@@ -1,32 +1,51 @@
-int cmp (const void *a, const void *b) {
-    return *(int*)a - *(int*)b;
-}
+// int cmp (const void *a, const void *b) {
+//     return *(int*)a - *(int*)b;
+// }
+// int getCommon(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+//     int arr[nums1Size+nums2Size];
+
+//     arr[0] = nums1[0];
+//     int count = 0;
+//     for (int i = 1; i < nums1Size; i++) {
+//         if (arr[count] != nums1[i]) {
+//             arr[++count] = nums1[i];
+//             // printf("%d - %d\n", count, arr[count]);
+//         }
+//     }
+
+//     arr[++count] = nums2[0];
+//     for (int i = 1; i < nums2Size; i++) {
+//         if (arr[count] != nums2[i]) {
+//             arr[++count] = nums2[i];
+//             // printf("%d - %d\n", count, arr[count]);
+//         }
+//     }
+
+//     qsort(arr, count+1, sizeof(int), cmp);
+
+//     for (int i = 1; i <= count; i++) {
+//         // printf("%d - %d\n", i, arr[i]);
+//         if (arr[i] == arr[i-1]) {
+//             return arr[i];
+//         }
+//     }
+
+//     return -1;
+// }
+
 int getCommon(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-    int arr[nums1Size+nums2Size];
+    int i = 0, j = 0;
 
-    arr[0] = nums1[0];
-    int count = 0;
-    for (int i = 1; i < nums1Size; i++) {
-        if (arr[count] != nums1[i]) {
-            arr[++count] = nums1[i];
-            // printf("%d - %d\n", count, arr[count]);
+    while (i < nums1Size && j < nums2Size) {
+        if (nums1[i] == nums2[j]) {
+            return nums1[i];
         }
-    }
 
-    arr[++count] = nums2[0];
-    for (int i = 1; i < nums2Size; i++) {
-        if (arr[count] != nums2[i]) {
-            arr[++count] = nums2[i];
-            // printf("%d - %d\n", count, arr[count]);
+        if (nums1[i] < nums2[j]) { 
+            i++;
         }
-    }
-
-    qsort(arr, count+1, sizeof(int), cmp);
-
-    for (int i = 1; i <= count; i++) {
-        // printf("%d - %d\n", i, arr[i]);
-        if (arr[i] == arr[i-1]) {
-            return arr[i];
+        else {
+            j++;
         }
     }
 
