c9da27ae8fe91b8524576034d4f30e8383072303
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-05-21 19:44:20 +0700

    Time: 255 ms (0%), Space: 34.3 MB (100%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.c
@@ -0,0 +1,38 @@
+int cmp (const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
+}
+
+int longestCommonPrefix(int* arr1, int arr1Size, int* arr2, int arr2Size) {
+    #define MAX_NUMBER (9) // 10^8
+    int* array = malloc(arr1Size * MAX_NUMBER * sizeof(int));
+    int ret = 0;
+    int k = 0;
+    for (int i = 0; i < arr1Size; i++) {
+        while (arr1[i]) {
+            array[k++] = arr1[i];
+            arr1[i] /= 10;
+        }
+    }
+
+    qsort(array, k, sizeof(int), cmp);
+
+    for (int i = 0; i < arr2Size; i++) {
+        while (arr2[i]) {
+            if (bsearch(&arr2[i], array, k, sizeof(int), cmp)) {
+                int k = 0; 
+                int val = arr2[i];
+                while (val) {
+                    val /= 10;
+                    k++;
+                }
+
+                if (k > ret) {
+                    ret = k;
+                } 
+            }
+            arr2[i] /= 10;
+        }
+    }
+
+    return ret;
+}
