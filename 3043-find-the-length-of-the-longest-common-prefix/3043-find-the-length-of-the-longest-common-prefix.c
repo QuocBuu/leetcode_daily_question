9bb7edd1f6222caf290ad0064321d73cf0dcbda5
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-05-21 19:46:43 +0700

    Time: 147 ms (100%), Space: 186.2 MB (0%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.c
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.c
@@ -1,40 +1,70 @@
+#define HASH_SIZE 200003
 
-int cmp (const void* a, const void* b) {
-    return *(int*)a - *(int*)b;
+typedef struct Node {
+    int val;
+    struct Node* next;
+} Node;
+
+unsigned int hashKey(int x) {
+    return ((unsigned int)x) % HASH_SIZE;
 }
 
+bool contains(Node** set, int x) {
+    unsigned int h = hashKey(x);
+    Node* cur = set[h];
+
+    while (cur) {
+        if (cur->val == x)
+            return true;
+
+        cur = cur->next;
+    }
+
+    return false;
+}
+
+void insert(Node** set, int x) {
+    if (contains(set, x))
+        return;
+
+    unsigned int h = hashKey(x);
+
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->val = x;
+    node->next = set[h];
+    set[h] = node;
+}
+
+int digitLen(int x) {
+    int len = 0;
+
+    while (x > 0) {
+        len++;
+        x /= 10;
+    }
+    return len;
+}
 int longestCommonPrefix(int* arr1, int arr1Size, int* arr2, int arr2Size) {
-    #define MAX_NUMBER (9) // 10^8
-    int* array = malloc(arr1Size * MAX_NUMBER * sizeof(int));
-    int ret = 0;
-    int k = 0;
+    Node** prefix = (Node**)calloc(HASH_SIZE, sizeof(Node*));
     for (int i = 0; i < arr1Size; i++) {
-        while (arr1[i]) {
-            array[k++] = arr1[i];
-            arr1[i] /= 10;
+        int a = arr1[i];
+        while (a > 0) {
+            if (contains(prefix, a))
+                break;
+            insert(prefix, a);
+            a /= 10;
         }
     }
-
-    qsort(array, k, sizeof(int), cmp);
-
+    int res = 0;
     for (int i = 0; i < arr2Size; i++) {
-        while (arr2[i]) {
-            if (bsearch(&arr2[i], array, k, sizeof(int), cmp)) {
-                int k = 0; 
-                int val = arr2[i];
-                while (val) {
-                    val /= 10;
-                    k++;
-                }
-
-                if (k > ret) {
-                    ret = k;
-                } 
+        int b = arr2[i];
+        while (b > res) {
+            if (contains(prefix, b)) {
+                res = b;
+                break;
             }
-            arr2[i] /= 10;
+            b /= 10;
         }
     }
-
-    free(array);
-    return ret;
+   return res ? digitLen(res) : 0;
 }
