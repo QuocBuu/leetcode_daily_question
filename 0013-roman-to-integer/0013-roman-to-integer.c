31cdeb0a12e19e1b4a69594d29d06acc57738c6c
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-05-26 19:03:57 +0700

    Time: 0 ms (100%), Space: 11.3 MB (21.86%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -0,0 +1,46 @@
+int romanToInt(char* s) {
+    int len = strlen(s);
+    int sum = 0;
+    for(int i = 0; i < len; i++) {
+        switch (s[i]) {
+            case 'I': {
+                if (i != len-1 && (s[i+1] == 'V' || s[i+1] == 'X')) {
+                    sum -= 1;
+                }
+                else {
+                    sum += 1;
+                }
+            } break;
+            case 'V': {
+                sum += 5;
+            } break;
+            case 'X': {
+                if (i != len-1 && (s[i+1] == 'L' || s[i+1] == 'C')) {
+                    sum -= 10;
+                }
+                else {
+                    sum += 10;
+                }
+            } break;
+            case 'L': {
+                sum += 50;
+            } break;
+            case 'C': {
+                if (i != len-1 && (s[i+1] == 'D' || s[i+1] == 'M')) {
+                    sum -= 100;
+                }
+                else {
+                    sum += 100;
+                }
+            } break;
+            case 'D': {
+                sum += 500;
+            } break;
+            case 'M': {
+                sum += 1000;
+            } break;
+            default: break;
+        }
+    }
+    return sum;
+}
