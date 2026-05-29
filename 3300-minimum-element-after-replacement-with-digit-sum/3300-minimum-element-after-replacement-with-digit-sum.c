7d27d3c168e267cd65e36c6beace45d8b6b4b793
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-05-29 08:54:00 +0700

    Time: 0 ms (100%), Space: 9.6 MB (88.61%) - LeetHub

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.c
@@ -0,0 +1,17 @@
+int minElement(int* nums, int numsSize) {
+    int ret = INT_MAX;
+    for (int i = 0; i < numsSize; i++) {
+        int val = nums[i];
+        nums[i] = 0;
+        while (val) {
+            nums[i] += val % 10;
+            val /= 10;
+        }
+
+        if (nums[i] < ret) {
+            ret = nums[i];
+        }
+    }
+
+    return ret;
+}
