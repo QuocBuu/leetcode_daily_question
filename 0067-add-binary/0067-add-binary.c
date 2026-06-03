5e2dd13389a933a524eaf0bb37cfea1d617c81ec
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-06-03 08:47:25 +0700

    Time: 0 ms (100%), Space: 8.9 MB (45%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.c
@@ -0,0 +1,51 @@
+char* addBinary(char* a, char* b) {
+    int lenA = strlen(a);
+    int lenB = strlen(b);
+    int maxLen = (lenA > lenB ? lenA : lenB) + 2;
+    char *ret = malloc(sizeof(char) * (maxLen));
+    memset(ret, '0', sizeof(char)*maxLen);
+    int rem = 0;
+    ret[--maxLen] = '\0';
+    while (lenA || lenB) {
+        int valA = 0;
+        int valB = 0;
+        if (lenA && a[lenA-1] == '1') {
+            valA = 1;
+        }
+        if (lenB && b[lenB-1] == '1') {
+            valB = 1;
+        }
+        
+        int val = valA + valB + rem;
+        lenA ? lenA-- : lenA;
+        lenB ? lenB-- : lenB;
+        rem = 0;
+        if (val == 0) {
+            ret[--maxLen] = '0';
+        }
+        else if (val == 1) {
+            ret[--maxLen] = '1';
+        }
+        else if (val == 2) {
+            ret[--maxLen] = '0';
+            rem = 1;
+        }
+        else if (val == 3) {
+            ret[--maxLen] = '1';
+            rem = 1;
+        }
+        
+    }
+
+    if (rem && maxLen) {
+        ret[--maxLen] = '1';
+    }
+    else {
+        size_t size = strlen(ret);
+        char *ret2 = malloc(size);
+        memcpy(ret2, ret + 1, size);
+        free(ret);
+        return ret2;
+    }
+    return ret;
+}
