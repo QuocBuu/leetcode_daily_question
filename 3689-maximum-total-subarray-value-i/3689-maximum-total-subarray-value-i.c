f02981aa862aff9b04ae87f9cd61be221af536b5
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-06-09 10:06:54 +0700

    Time: 0 ms (100%), Space: 16.2 MB (0%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.c
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.c
@@ -1,14 +1,20 @@
-int cmp (const void* a, const void* b) {
-    return *(int*)a - *(int*)b;
-}
+// int cmp (const void* a, const void* b) {
+//     return *(int*)a - *(int*)b;
+// }
 long long maxTotalValue(int* nums, int numsSize, int k) {
     if (numsSize <= 1) {
         return 0;
     }
 
-    qsort(nums, numsSize, sizeof(int), cmp);
+    // qsort(nums, numsSize, sizeof(int), cmp);
+    int max = INT_MIN;
+    int min = INT_MAX;
+    for (int i = 0; i < numsSize; i++) {
+        if (max < nums[i]) {max = nums[i];}
+        if (min > nums[i]) {min = nums[i];}
+    }
 
-    long long ret = (long long)(nums[numsSize-1] - nums[0]) * k;
+    long long ret = (long long)(max - min) * k;
 
     return ret;
 }
