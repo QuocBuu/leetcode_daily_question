89b717b976ad9a1b20abd894790f209d157bd354
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-06-15 01:36:31 +0700

    Time: 0 ms (100%), Space: 9.3 MB (77.27%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void dfs(struct TreeNode* root, int* arr, int *size) {
+    if (root == NULL) {
+        return;
+    }
+    dfs(root->left, arr, size);
+    arr[(*size)++] = root->val;
+    dfs(root->right, arr, size); 
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int *ret = (int*) malloc(sizeof(int)*101);
+    *returnSize = 0;
+    dfs(root, ret, returnSize);
+
+    return ret;
+}
