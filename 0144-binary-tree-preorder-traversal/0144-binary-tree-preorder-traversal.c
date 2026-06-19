5630c7b8796201b037d190a91167ff939a0b1ea9
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-06-20 01:51:56 +0700

    Time: 0 ms (100%), Space: 9.2 MB (78.24%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void bfs (int* arr, struct TreeNode *root, int* size) {
+    if (root == NULL) {
+        return;
+    }
+
+    arr[*size] = root->val;
+    (*size)++;
+
+    bfs(arr, root->left, size);
+    bfs(arr, root->right, size);
+}
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+    int *ret = malloc(sizeof(int) * 100);
+
+    int size = 0;
+    bfs(ret, root, &size);
+    *returnSize = size;
+    return ret;
+}
