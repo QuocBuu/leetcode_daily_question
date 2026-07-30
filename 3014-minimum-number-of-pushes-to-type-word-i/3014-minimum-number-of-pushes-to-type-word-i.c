ed4da796faa8cb01da574e0653b2c53e47e6ea66
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-07-30 13:32:14 +0700

    Time: 5 ms (0%), Space: 9 MB (0%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.c
@@ -0,0 +1,27 @@
+int cmp (const void* a, const void* b) {
+    return *(int*)b - *(int*)a;
+}
+
+int minimumPushes(char* word) {
+    #define NUM_CHAR (26)
+    int len = strlen(word);
+    int arr[NUM_CHAR] = {0};
+
+    for (int i = 0; i < len; i++) {
+        arr[word[i] - 'a']++;
+    }
+
+    qsort(arr, NUM_CHAR, sizeof(int), cmp);
+
+    int ret = 0;
+    for (int i = 0; i < NUM_CHAR; i++) {
+        int cnt = (1 + (i / 8));
+        ret += arr[i] * cnt;
+        if (arr[i] == 0) {
+            break;
+        }
+        printf("%d - %d - %c\n", ret, cnt, i + 'a');
+    }
+
+    return ret;
+}
