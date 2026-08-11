ac20928995748ec2ea95db06d9e959b76feb4374
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-08-11 07:02:30 +0700

    Time: 3 ms (100%), Space: 9.2 MB (36.36%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.c
+++ 1510-stone-game-iv/1510-stone-game-iv.c
@@ -1,15 +1,17 @@
 bool winnerSquareGame(int n) {
-    #define NUM_MAX (100001)
-    int dp[NUM_MAX] = {0};
+    bool* dp = malloc(sizeof(bool) * (n + 1));
+    memset(dp, 0, sizeof(bool) * (n + 1));
     for (int i = 0; i < n; i++) {
         if (dp[i]) {
             continue;
         }
         
         for (int j = 1;  i + j * j <= n; j++) {
-            dp[i + j*j] = 1;
+            dp[i + j*j] = true;
         }
     }
 
-    return dp[n];
+    bool ret = dp[n]; 
+    free(dp);
+    return ret;
 }
