b3a8c5d0fe91a4be2643fba8316178ee807325a1
Phan Quoc Buu <118478658+QuocBuu@users.noreply.github.com>
2026-08-25 23:18:38 +0700

    Time: 4 ms (5.26%), Space: 9.8 MB (39.47%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.c
@@ -0,0 +1,22 @@
+int cmp (const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
+}
+
+int missingMultiple(int* nums, int numsSize, int k) {
+    qsort(nums, numsSize, sizeof(int), cmp);
+    int val = k;
+    for (int i = 0; i < numsSize; i++) {
+        // printf("%d-", nums[i]);
+        if (nums[i] < val) {
+            continue;
+        }
+        else if (nums[i] == val) {
+            val += k;
+        }
+        else {
+            return val;
+        }
+    }
+
+    return val;
+}
